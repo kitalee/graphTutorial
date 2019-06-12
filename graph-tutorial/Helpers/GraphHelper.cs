e6bcc8c4ac42169e4fe4b92c37ed2df66e39ec4c
DongGeon Lee <donggeon.lee@keymedia.com>
2019-06-12 15:05:51 +1000

    init

---
+++ graph-tutorial/Helpers/GraphHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Graph;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace graph_tutorial.Helpers
+{
+    public static class GraphHelper
+    {
+        public static async Task<User> GetUserDetailsAsync(string accessToken)
+        {
+            // Bearer Tokens are the predominant type of access token used with OAuth 2.0.
+            var graphClient = new GraphServiceClient(
+                new DelegateAuthenticationProvider(
+                    async (requestMessage) =>
+                    {
+                        requestMessage.Headers.Authorization =
+                            new AuthenticationHeaderValue("Bearer", accessToken);
+                    }));
+
+            return await graphClient.Me.Request().GetAsync();
+        }
+    }
+}
---
+++ graph-tutorial/Startup.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(graph_tutorial.Startup))]
+
+//namespace graph_tutorial
+//{
+//    public class Startup
+//    {
+//        public void Configuration(IAppBuilder app)
+//        {
+//            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+//        }
+//    }
+//}
+
+namespace graph_tutorial
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
